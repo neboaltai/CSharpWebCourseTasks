5c6e633542d87bf5527c607853f30819227bf48e
Mikhail Blinov <spasibo1989@gmail.com>
2023-03-29 14:34:15 +0700

    Добавлены проекты Gamble и PersonLibrary

---
+++ Gamble/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using PersonLibrary;
+
+namespace Gamble
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var random = new Random();
+
+            const double odds = 0.75;
+            
+            var player = new Person("Michael", 100);
+
+            Console.WriteLine($"Welcome to the casino. The odds are {odds}");
+            Console.WriteLine();
+
+            while (player.Cash > 0)
+            {
+                Console.WriteLine(player);
+                Console.Write("How much do you want to bet: ");
+                
+                if (int.TryParse(Console.ReadLine(), out var bet))
+                {
+                    if (bet > 0)
+                    {
+                        var number = random.NextDouble();
+
+                        if (number > odds)
+                        {
+                            Console.WriteLine($"You win {bet * 2}!");
+                            Console.WriteLine();
+                            player.ReceiveCash(bet);
+
+                            continue;
+                        }
+
+                        Console.WriteLine("Bad luck, you lose.");
+                        Console.WriteLine();
+                        player.GiveCash(bet);
+
+                        continue;
+                    }
+                }
+
+                Console.WriteLine($"{bet} isn't a valid amount");
+            }
+
+            Console.WriteLine("The house always wins.");
+        }
+    }
+}
---
+++ PersonLibrary/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PersonLibrary
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Cash { get; set; }
+
+        public Person() { }
+
+        public Person(string name, int cash)
+        {
+            Name = name;
+            Cash = cash;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} has ${Cash}.";
+        }
+
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine($"{Name} says: {amount} isn't a valid amount");
+
+                return 0;
+            }
+            else if (amount > Cash)
+            {
+                Console.WriteLine($"{Name} says: I don't have enough cash to give you ${amount}");
+
+                return 0;
+            }
+
+            Cash -= amount;
+
+            return amount;
+        }
+
+        public void ReceiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine($"{Name} says: {amount} isn't an amount I'll take");
+            }
+
+            Cash += amount;
+        }
+    }
+}
