5cf93d35299243f66304fee34bd1d16341ab0ef5
Mikhail Blinov <spasibo1989@gmail.com>
2023-04-09 18:57:20 +0700

    Добавление проекта ConditionalCompilation

---
+++ ConditionalCompilation/Program.cs
@@ -0,0 +1,21 @@
+﻿const int x = 1;
+
+#region Boxing
+static void Method1()
+{
+    Console.WriteLine("Result: {0}", x);
+}
+#endregion
+
+#region NotBoxing
+static void Method2()
+{
+    Console.WriteLine("Result: {0}", x.ToString());
+}
+#endregion
+
+#if DEBUG
+Method1();
+#else
+Method2();
+#endif
