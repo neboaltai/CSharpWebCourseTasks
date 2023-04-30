6c3964eba519a4808a3058d157e34cccb3165e1b
Mikhail Blinov <spasibo1989@gmail.com>
2023-04-30 23:51:05 +0700

    Добавление проекта VectorTask

---
+++ VectorTask/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace VectorTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector vector1 = new Vector(new Vector(5));
+
+            double temp = vector1.GetComponent(0);
+
+            temp += 4.5;
+            vector1.SetComponent(0, temp);
+
+            Vector vector2 = new Vector(new double[] { -3, 5, 8 });
+
+            Console.WriteLine("Заданы:");
+            Console.WriteLine($"- {vector1.GetSize()}-мерный вектор v1 = {vector1}");
+            Console.WriteLine($"- {vector2.GetSize()}-мерный вектор v2 = {vector2}");
+
+            Console.WriteLine("Вычислить и найти длину вектора: -(6 * (v1 + v2) - v2)");
+            Console.WriteLine();
+
+            Console.WriteLine("Решение:");
+            Console.Write("1. Прибавление вектора v2 к v1: v1 + v2 = ");
+            vector1.Add(vector2);
+            Console.WriteLine(vector1);
+
+            Console.Write("2. Умножение вектора(1) на скалярную величину: 6 * (v1 + v2) = ");
+            vector1.MultiplyByScalar(6);
+            Console.WriteLine(vector1);
+
+            Console.Write("3. Вычитание вектора v2 из вектора(2): 6 * (v1 + v2) - v2 = ");
+            vector1.Subtract(vector2);
+            Console.WriteLine(vector1);
+
+            Console.Write("4. Умножение вектора(3) на -1 (разворот вектора): -(6 * (v1 + v2) - v2) = ");
+            vector1.Reverse();
+            Console.WriteLine(vector1);
+
+            Console.WriteLine($"5. Длина вектора: {vector1.GetLength():f2}");
+
+            Vector resultVector = new Vector(5, new double[] { -12, -25, -40 });
+
+            Console.WriteLine();
+            Console.Write($"Сравнение с ответом: {resultVector} - {vector1.Equals(resultVector)}, ");
+            Console.WriteLine(vector1.GetHashCode() == resultVector.GetHashCode());
+
+            Console.WriteLine();
+            Console.Write("Вектор v3 = v1 + v2 = ");
+            Vector vector3 = Vector.GetSum(resultVector, vector2);
+            Console.WriteLine(vector3);
+
+            Console.Write("Вектор v4 = v1 - v2 = ");
+            Vector vector4 = Vector.GetDifference(resultVector, vector2);
+            Console.WriteLine(vector4);
+
+            Console.Write("Длина вектора v5: v1 * v2 = ");
+            double vector5Length = Vector.GetDotProduct(resultVector, vector2);
+            Console.WriteLine(vector5Length);
+        }
+    }
+}
---
+++ VectorTask/Vector.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Linq;
+
+namespace VectorTask
+{
+    public class Vector
+    {
+        private double[] _components;
+
+        public Vector(int size) : this(size, null) { }
+
+        public Vector(double[] components) : this(components.Length, components) { }
+
+        public Vector(Vector vector) : this(vector._components.Length, vector._components) { }
+
+        public Vector(int size, double[] components)
+        {
+            if (size <= 0)
+            {
+                throw new ArgumentException($"Parameter value {size} is invalid. The count of components must be > 0", nameof(size));
+            }
+
+            _components = new double[size];
+
+            if (components != null && components.Length != 0)
+            {
+                Array.Copy(components, _components, Math.Min(size, components.Length));
+            }
+        }
+
+        public int GetSize()
+        {
+            return _components.Length;
+        }
+
+        public override string ToString()
+        {
+            return $"{{{string.Join(", ", _components)}}}";
+        }
+
+        public void Add(Vector vector)
+        {
+            if (vector._components.Length > _components.Length)
+            {
+                Array.Resize(ref _components, vector._components.Length);
+            }
+
+            for (var i = 0; i < vector._components.Length; i++)
+            {
+                _components[i] += vector._components[i];
+            }
+        }
+
+        public void Subtract(Vector vector)
+        {
+            if (vector._components.Length > _components.Length)
+            {
+                Array.Resize(ref _components, vector._components.Length);
+            }
+
+            for (var i = 0; i < vector._components.Length; i++)
+            {
+                _components[i] -= vector._components[i];
+            }
+        }
+
+        public void MultiplyByScalar(double number)
+        {
+            for (var i = 0; i < _components.Length; i++)
+            {
+                _components[i] *= number;
+            }
+        }
+
+        public void Reverse()
+        {
+            MultiplyByScalar(-1);
+        }
+
+        public double GetLength()
+        {
+            var componentSquaresSum = _components.Sum(component => component * component);
+
+            return Math.Sqrt(componentSquaresSum);
+        }
+
+        public double GetComponent(int index)
+        {
+            return _components[index];
+        }
+
+        public void SetComponent(int index, double component)
+        {
+            _components[index] = component;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(obj, this))
+            {
+                return true;
+            }
+
+            if (obj is null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            var vector = (Vector)obj;
+
+            if (_components.Length != vector._components.Length)
+            {
+                return false;
+            }
+
+            return !_components.Where((t, i) => t != vector._components[i]).Any();
+        }
+
+        public override int GetHashCode()
+        {
+            const int prime = 17;
+
+            return _components.Aggregate(1, (current, e) => prime * current + e.GetHashCode());
+        }
+
+        public static Vector GetSum(Vector vector1, Vector vector2)
+        {
+            var result = new Vector(vector1);
+
+            result.Add(vector2);
+
+            return result;
+        }
+
+        public static Vector GetDifference(Vector vector1, Vector vector2)
+        {
+            var result = new Vector(vector1);
+
+            result.Subtract(vector2);
+
+            return result;
+        }
+
+        public static double GetDotProduct(Vector vector1, Vector vector2)
+        {
+            double result = 0;
+            double minVectorSize = Math.Min(vector1._components.Length, vector2._components.Length);
+
+            for (var i = 0; i < minVectorSize; i++)
+            {
+                result += vector1._components[i] * vector2._components[i];
+            }
+
+            return result;
+        }
+    }
+}
