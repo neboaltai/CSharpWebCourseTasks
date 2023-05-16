087eacb653b8e4397fe999799bc25e407509e151
Mikhail Blinov <spasibo1989@gmail.com>
2023-05-16 20:25:17 +0700

    Добавление проекта LogTask

---
+++ LogTask/Program.cs
@@ -0,0 +1,57 @@
+﻿using LogTask;
+using NLog;
+
+LogManager.Setup().LoadConfiguration(builder =>
+{
+    builder.ForLogger().FilterMinLevel(LogLevel.Debug).WriteToConsole();
+    builder.ForLogger().FilterMinLevel(LogLevel.Info).WriteToFile(fileName: "../../../logs.txt");
+});
+
+var logger = LogManager.GetCurrentClassLogger();
+
+var swordDamage = new SwordDamage();
+
+var random = new Random();
+
+var rules = "";
+
+try
+{
+    using var reader = new StreamReader("../../../rules.txt");
+
+    rules = reader.ReadToEnd();
+}
+catch (IOException e)
+{
+    logger.Error(e);
+}
+
+while (true)
+{
+    Console.Write(rules);
+
+    var key = Console.ReadKey(false).KeyChar;
+
+    Console.WriteLine();
+
+    if (key is not '0' and not '1' and not '2' and not '3')
+    {
+        break;
+    }
+
+    swordDamage.Roll = RollDice(random);
+
+    swordDamage.IsMagic = key is '1' or '3';
+
+    swordDamage.IsFlaming = key is '2' or '3';
+
+    Console.WriteLine();
+    Console.WriteLine($"Rolled {swordDamage.Roll} for {swordDamage.Damage} HP");
+
+    Console.WriteLine();
+}
+
+static int RollDice(Random random)
+{
+    return random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+}
---
+++ LogTask/SwordDamage.cs
@@ -0,0 +1,77 @@
+﻿using NLog;
+
+namespace LogTask
+{
+    internal class SwordDamage
+    {
+        private readonly ILogger _logger;
+        private int _roll;
+        private bool _isMagic;
+        private bool _isFlaming;
+
+        private const int BaseDamage = 3;
+        private const int FlameDamage = 2;
+        private const double MagicMultiplier = 1.75;
+
+        public int Damage { get; private set; }
+
+        public int Roll
+        {
+            get => _roll;
+
+            set
+            {
+                _roll = value;
+
+                CalculateDamage();
+            }
+        }
+
+        public bool IsFlaming
+        {
+            get => _isFlaming;
+
+            set
+            {
+                _isFlaming = value;
+
+                _logger.Debug("Set flame damage");
+
+                CalculateDamage();
+            }
+        }
+
+        public bool IsMagic
+        {
+            get => _isMagic;
+
+            set
+            {
+                _isMagic = value;
+
+                _logger.Debug("Set magic damage");
+
+                CalculateDamage();
+            }
+        }
+
+        public SwordDamage()
+        {
+            _logger = LogManager.GetCurrentClassLogger();
+        }
+
+        private void CalculateDamage()
+        {
+            Damage = BaseDamage;
+
+            Damage += _isMagic ? (int)(_roll * MagicMultiplier) : _roll;
+
+            if (_isFlaming)
+            {
+                Damage += FlameDamage;
+            }
+
+            _logger.Debug("Damage calculated");
+        }
+    }
+}
